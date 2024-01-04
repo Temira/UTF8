b8eff74091d55bc1c16b3c387b4ce7523706f2e8
tkoenig <trk720@yahoo.com>
2024-01-04 15:49:48 -0500

    working on filling the utf8 binary template

--- 3.c
+++ 3.c
@@ -3,22 +3,23 @@
 
 int isUtf8_StartByte(unsigned char byte); //determines if a byte is a valid start byte
 int isUtf8_ContinuationByte(unsigned char byte); //determines if a byte is a valid continuation byte
-int hexToBinary(const char *hexa, char *binarynum); //converts hex to binary (duh)
+int hexToBinary(const char *hexa, unsigned char *binarynum); //converts hex to binary (duh)
 void printBinaryEquivalent(const unsigned char *stri);
 int checkCharLength(const unsigned char *stri); //determines how many UTF-8 Hex pairs there will be based on start byte
 int my_utf8_strlen(char *string); //uses start bytes
-int length(char *string); //uses nul to locate end of string
-int binaryToDecimal(const char *binary); //converts binary to decimal
+//int length(char *string); //uses nul to locate end of string
+int binaryToDecimal(unsigned const char *binary); //converts binary to decimal
 int determineRange(int number); //determines what range the unicode string falls into
-int fillTemp1(); //fills one byte template
+//int fillTemp1(); //fills one byte template
 int fillTemp2(); //fills two byte template
 int fillTemp3(); //fills three byte template
 int fillTemp4(); // fills four byte template
 int codepointToUTF(const unsigned char *stri); //converts unicode codepoint to equivalent UTF-8 hex
+int stripLeadingZeros(unsigned char *binaryString); //
 
 
 
-int hexToBinary(const char *hexa, char *binarynum){
+int hexToBinary(const char *hexa, unsigned char *binarynum){
     long int i = 0;
     const char *hexaPointer;
     //printf("%c", hexa[0]);
@@ -164,7 +165,7 @@ int hexToBinary(const char *hexa, char *binarynum){
     printf("Binary equivalent is: %s", binarynum);
     return 0;
 }
-int binaryToDecimal(const char *binary) {
+int binaryToDecimal(unsigned const char *binary) {
     int decimal = 0;
 
     // Calculate the length of the binary string
@@ -198,11 +199,89 @@ int determineRange(int number) {
         return 0;
     }
 }
-int fillTemp1(){
-    printf("\nt1");
+
+int stripLeadingZeros(unsigned char *binaryString) {
+    if (binaryString == NULL) {
+        // Handle NULL pointer accordingly
+        return -1;
+    }
+    int leadingZeros = 0;
+    // Find the number of leading zeros
+    while (binaryString[leadingZeros] == '0' && binaryString[leadingZeros] != '\0') {
+        leadingZeros++;
+    }
+    // Shift over however many leading zeros there are
+    int i;
+    for (i = 0; binaryString[i + leadingZeros] != '\0'; i++) {
+        binaryString[i] = binaryString[i + leadingZeros];
+    }
+    binaryString[i] = '\0';
 }
-int fillTemp2(){
-    printf("\nt2");
+
+int length(unsigned char *str){
+    int currentLength = 0;
+    // Calculate the current length of the string
+    while (str[currentLength] != '\0') {
+        currentLength++;
+    }
+    return currentLength;
+}
+//
+int addLeadingZeros(unsigned char *binaryString, int targetLength) {
+    if (binaryString == NULL || targetLength <= 0) {
+        return -1;
+    }
+    int currentLength = length(binaryString);
+    if (currentLength >= targetLength) {
+        // The string is already longer or equal to the target length
+        return 0;
+    }
+    // Calculate the number of zeros to add
+    int zerosToAdd = targetLength - currentLength;
+    // Make room for the leading zeros
+    for (int i = currentLength; i >= 0; i--) {
+        binaryString[i + zerosToAdd] = binaryString[i];
+    }
+    // Add the leading zeros
+    for (int i = 0; i < zerosToAdd; i++) {
+        binaryString[i] = '0';
+    }
+}
+
+
+int fillTemp1(unsigned char * binaryNum, unsigned char * newNumber){
+    printf("\n1\n");
+    //get significant numbers to make sure it fits into the template
+    stripLeadingZeros(binaryNum);
+    //should be a binary of length 7 for it to fit well into the template
+    addLeadingZeros(binaryNum, 7);
+    newNumber[0] = '0';
+    for (int i=0; i<7; i++){
+        newNumber[i+1] = binaryNum[i];
+    }
+    newNumber[8] ='\0';
+    return 0;
+}
+
+int fillTemp2(unsigned char * binaryNum, unsigned char * newNumber){
+    printf("\n2\n");
+    //get significant numbers to make sure it fits into the template
+    stripLeadingZeros(binaryNum);
+    //should be a binary of length 11 for it to fit well into the template
+    addLeadingZeros(binaryNum, 11);
+    newNumber[0] = '1';
+    newNumber[1] = '1';
+    newNumber[2] = '0';
+    for (int i = 0, j = 3; i < 6; j++, i++){
+        newNumber[j] = binaryNum[i];
+    }
+    newNumber[8] = '1';
+    newNumber[9] = '0';
+    for (int i = 6, j = 10; i < 12; j++, i++){
+        newNumber[j] = binaryNum[i];
+    }
+    newNumber[8] ='\0';
+    return 0;
 }
 int fillTemp3(){
     printf("\nt3");
@@ -211,7 +290,8 @@ int fillTemp4(){
     printf("\nt4");
 }
 int codepointToUTF(const unsigned char *stri) {
-    char binary[MAX] = {0};
+    unsigned char binary[MAX] = {0};
+    unsigned char newBinary[MAX] = {0};
     hexToBinary(stri, binary);
     int i = binaryToDecimal(binary);
     //at this point, i contains the decimal equivalent of the unicode codepoint
@@ -219,10 +299,12 @@ int codepointToUTF(const unsigned char *stri) {
     printf("Needs %d bytes", range);
     //Fill appropriate template
     if (range == 1){
-        fillTemp1();
+        fillTemp1(binary, newBinary);
+        printf("new %s", newBinary);
     }
     else if (range == 2){
-        fillTemp2();
+        fillTemp2(binary, newBinary);
+        printf("new %s", newBinary);
     }
     else if (range == 3){
         fillTemp3();
@@ -241,10 +323,10 @@ int main(void) {
 //
 //    const char* hex1 = "1F";
 //    const char* hex2 = "0x2A";
-    const char* hex3 = "\\u10348";
+    const char* hex3 = "\\u0081";
 //    hexToBinary(hex3, binary);
 //    const char *character = "!";
-    hexToBinary(hex3, binary);
+    //hexToBinary(hex3, binary);
     codepointToUTF(hex3);
 //    int len = checkCharLength((const unsigned char *) character);
 //    //printBinaryEquivalent((const unsigned char *) character);
